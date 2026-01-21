cb2932ce7b8972ce3263274820ce18e73e21c532
solomon monotilla <solomon.monotilla@gmail.com>
2026-01-21 17:59:57 +0800

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const API_URL = 'https://raw.githubusercontent.com/suyogshiftcare/jsontest/main/available.json';
+export const BOOKINGS_STORAGE_KEY = '@shiftcare_bookings';
+export const DAYS_AHEAD = 14;
